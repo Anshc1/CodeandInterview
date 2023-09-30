721aef0680a3ce8e0c27b8a9158def1fcd84c21d
anshc1 <anshchaturvadi2000@gmail.com>
2023-10-01 00:11:57 +0530

    secondcommit

--- Backend/app.js
+++ Backend/app.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 5000
-
-
+const cors = require('cors')
+const http = require('http')
 const connectToDb = require("./dataBaseconnection"); 
-connectToDb(); 
 
+app.use(cors()); 
+const server = http.createServer(app); 
+connectToDb(); 
 
 app.get('/' , (req ,res)=>{
     res.send("hello"); 
 })
-app.listen((port) , ()=>{
+app.use('./routes/login');
+app.use('./routes/signup');
+
+server.listen((port) , ()=>{
     console.log('running'); 
 })
